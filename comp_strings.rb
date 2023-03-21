9ce71ac65cfb8c717e909247a2193bdcbba0013e
danmvidrogpe <daniel.vidal@rogpe.tech>
2023-03-21 10:10:44 -0300

    compare strings

---
+++ comp_strings.rb
@@ -0,0 +1,11 @@
+puts "Digite a primeira frase: "
+phrase1 = gets.chomp
+
+puts "Digite a segunda frase: "
+phrase2 = gets.chomp
+
+if phrase1 == phrase2
+		puts "Iguais"
+else
+	puts "Diferentes"
+end
