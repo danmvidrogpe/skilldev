5ce3a038376d6c2e9c0c2109113a5af6f08a5526
danmvidrogpe <daniel.vidal@rogpe.tech>
2023-03-21 10:18:18 -0300

    verifyied if sum is biggest 10

---
+++ sum_biggest.rb
@@ -0,0 +1,22 @@
+puts "Quantos números deseja inserir? "
+count = gets.chomp.to_i
+
+puts "Digite os números: "
+
+numbers = []
+index = 0
+
+while index < count
+	numbers[index] = gets.chomp.to_i
+	index += 1
+end
+
+sum = 0
+
+numbers.each do |num|
+	if num > 10
+		sum = sum + num
+	end
+end
+
+puts "A soma dos números maiores que 10 é #{sum}"
