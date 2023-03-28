8c418f5b7bb50e4b2ea2bc26c5fa187694d0522d
danmvidrogpe <daniel.vidal@rogpe.tech>
2023-03-28 09:55:06 -0300

    bubble sort dynamic  insert

--- bubble.rb
+++ bubble.rb
@@ -1,19 +1,21 @@
-array = [10, 40, 32, 20, 8]
-
-i = 0
-array.each do |num|
-	position = 0
-	while position < array.length - 1
-		if array[position] < array[position+1]
-			smallest = array[position]
-		else
-			smallest = array[position+1]
-			array[position+1] = array[position]
-			array[position] = smallest
-		end
-		position+=1
-	end
-	i += 1
+list_numbers = (1..10).map do |num|
+  rand(0..100)
 end
 
-puts array
+index = 0
+list_numbers.each do |num|
+	position = 0
+	while position < list_numbers.length - 1
+		if list_numbers[position] < list_numbers[position + 1]
+			smallest = list_numbers[position] 
+		else
+			smallest = list_numbers[position + 1]
+			list_numbers[position + 1] = list_numbers[position]
+			list_numbers[position] = smallest
+		end
+		position += 1
+	end
+	index += 1
+end
+
+puts list_numbers
