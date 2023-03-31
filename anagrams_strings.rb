a38c72c21711646d35c66cd3ac5f4c7522d3a677
danmvidrogpe <daniel.vidal@rogpe.tech>
2023-03-31 09:20:36 -0300

    fixed the use of each

--- anagrams_strings.rb
+++ anagrams_strings.rb
@@ -7,23 +7,18 @@ phrase2 = gets.chomp.delete(' ').split('')
 count1 = 0
 count2 = 0
 
-phrase1.each_with_index do |char, position|
-	if phrase2.include?(char)
-		count1+=1
-	end
+phrase1.each do |char|
+  count1+=1	if phrase2.include?(char)
 end
 
-phrase2.each_with_index do |char, position|
-	if phrase1.include?(char)
-		count2+=1
-	end
+phrase2.each do |char|
+  count2+=1	if phrase1.include?(char)
 end
 
 sum_count = count1 + count2
 phrases_size = phrase1.length + phrase2.length
 
-if phrases_size == sum_count
-	puts "É Anagrama"
-else
-	puts "Não é anagrama"
-end
+
+return puts "É Anagrama" if phrases_size == sum_count
+
+puts "Não é anagrama"
